77db72b35760a24fa7335f95f59033608096c335
tom_glb <goodMorning_glb@fgm.com>
2019-07-18 13:57:53 +0800

    TestGit3--项目

--- src/main/java/com/itguigu/git/Happy.java
+++ src/main/java/com/itguigu/git/Happy.java
@@ -3,7 +3,7 @@ package com.itguigu.git;
 public class Happy {
 	
    public static void main(String[] args) {
-	   
+	  System.out.println("做了些改变03"); 
 	  System.out.println("做了些改变02");
 	  System.out.println("做了些改变01");
       //修改后按Ctrl + Shift + 3
